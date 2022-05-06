2e7951509474987f73c35cfe6ceb27132225edc4
RFaska <93133836+playerRC@users.noreply.github.com>
2022-05-06 17:56:29 +0200

    Update teams.py

--- teams.py
+++ teams.py
@@ -1,5 +1,6 @@
-
-
+import requests
+import XPath as xp
+from bs4 import BeautifulSoup
 
 def homeTeam(url):
     l = []
@@ -12,5 +13,5 @@ def awayTeam(url):
     l = []
     for i in range(1, nbListes()+1):
         for j in range(1, nbElementsDansChaqueListe()[i-1]+1):
-            l.append(Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[3]/div/span[1]'))
+            l.append(xp.Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[3]/div/span[1]'))
     return l
