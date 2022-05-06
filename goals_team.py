b6a32fb159400ce22263a0d3af2188319107a5f2
RFaska <93133836+playerRC@users.noreply.github.com>
2022-05-06 18:57:39 +0200

    Update goals_team.py

--- goals_team.py
+++ goals_team.py
@@ -6,14 +6,20 @@ import nbElementsUL as nbu.
 def homeGoal(url):
   l = []
   for i in range(1, nbu.nbListes(url)+1):
-      for j in range(1, nbu.nbElementsDansChaqueListe(url)[i-1]+1):
-          l.append(int(xp.Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[2]/span/span[1]')))
+    for j in range(1, nbu.nbElementsDansChaqueListe(url)[i-1]+1):
+        if Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[2]/span/span[3]') == None:
+              l.append(None)
+        else:
+              l.append(int(xp.Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[2]/span/span[1]')))
   return l
 
 
 def awayGoal(url):
   l = []
   for i in range(1, nbu.nbListes(url)+1):
-      for j in range(1, nbu.nbElementsDansChaqueListe(url)[i-1]+1):
-          l.append(int(xp.Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[2]/span/span[3]')))
+    for j in range(1, nbu.nbElementsDansChaqueListe(url)[i-1]+1):
+      if Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[2]/span/span[3]') == None:
+              l.append(None)
+      else:
+              l.append(int(xp.Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[2]/span/span[3]')))
   return l
