7795d2bd06700e96f10f8a6fa2e30d84aebe3bcd
RFaska <93133836+playerRC@users.noreply.github.com>
2022-05-06 17:16:49 +0200

    Update scrap_teams.py

--- scrap_teams.py
+++ scrap_teams.py
@@ -5,6 +5,12 @@ from bs4 import BeautifulSoup
 r = requests.get('https://www.ligue1.fr/calendrier-resultats?seasonId=2021-2022&matchDay=6')
 soup = BeautifulSoup(r.content, 'html.parser')
 
+#nb de ul dans le div
+def nbListes():
+    s = soup.find('div', class_='calendar-widget-container')
+    s2 = s.find_all('ul')
+    return len(s2)
+
 s = soup.find('div', class_='calendar-widget-container')
 teams = s.find_all('div', class_ = 'club home')
 team_name = s.find_all('span')
