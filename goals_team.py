3b11723cf2c1afc1b5d066832befd3bad26fdfa3
RFaska <93133836+playerRC@users.noreply.github.com>
2022-05-06 18:24:37 +0200

    Update goals_team.py

--- goals_team.py
+++ goals_team.py
@@ -1,16 +1,19 @@
-
+import requests
+import XPath as xp
+from bs4 import BeautifulSoup
+import nbElementsUL as nbu.
 
 def homeGoal(url):
   l = []
-  for i in range(1, nbListes()+1):
-      for j in range(1, nbElementsDansChaqueListe()[i-1]+1):
+  for i in range(1, nbu.nbListes()+1):
+      for j in range(1, nbu.nbElementsDansChaqueListe()[i-1]+1):
           l.append(int(Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[2]/span/span[1]')))
   return l
 
 
 def awayGoal(url):
   l = []
-  for i in range(1, nbListes()+1):
-      for j in range(1, nbElementsDansChaqueListe()[i-1]+1):
+  for i in range(1, nbu.nbListes()+1):
+      for j in range(1, nbu.nbElementsDansChaqueListe()[i-1]+1):
           l.append(int(Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[2]/span/span[3]')))
   return l
