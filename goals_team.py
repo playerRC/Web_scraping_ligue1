d69bbf008ea9ee45e79878c8f64da990c143691b
RFaska <93133836+playerRC@users.noreply.github.com>
2022-05-06 18:26:47 +0200

    Update goals_team.py

--- goals_team.py
+++ goals_team.py
@@ -5,15 +5,15 @@ import nbElementsUL as nbu.
 
 def homeGoal(url):
   l = []
-  for i in range(1, nbu.nbListes()+1):
-      for j in range(1, nbu.nbElementsDansChaqueListe()[i-1]+1):
-          l.append(int(Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[2]/span/span[1]')))
+  for i in range(1, nbu.nbListes(url)+1):
+      for j in range(1, nbu.nbElementsDansChaqueListe(url)[i-1]+1):
+          l.append(int(xp.Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[2]/span/span[1]')))
   return l
 
 
 def awayGoal(url):
   l = []
-  for i in range(1, nbu.nbListes()+1):
-      for j in range(1, nbu.nbElementsDansChaqueListe()[i-1]+1):
-          l.append(int(Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[2]/span/span[3]')))
+  for i in range(1, nbu.nbListes(url)+1):
+      for j in range(1, nbu.nbElementsDansChaqueListe(url)[i-1]+1):
+          l.append(int(xp.Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[2]/span/span[3]')))
   return l
