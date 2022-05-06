906a9019ccf2e8f7095c96e321626a34c5c7b17a
RFaska <93133836+playerRC@users.noreply.github.com>
2022-05-06 17:57:14 +0200

    Update and rename home_teams.py to nbElementsUL.py

--- nbElementsUL.py
+++ nbElementsUL.py
@@ -16,12 +16,3 @@ def nbElementsDansChaqueListe():
         s3 = ul.find_all('li')
         l.append(len(s3))
     return l
-        
-def homeTeam(url):
-    l = []
-    for i in range(1, nbListes()+1):
-        for j in range(1, nbElementsDansChaqueListe()[i-1]+1):
-            l.append(xp.Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[1]/div/span[1]'))
-    return l
-
-
