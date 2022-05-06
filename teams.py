d0c170d3531f78acecb21b5f15b4ccbcc234c988
RFaska <93133836+playerRC@users.noreply.github.com>
2022-05-06 17:55:48 +0200

    Update teams.py

--- teams.py
+++ teams.py
@@ -1,6 +1,13 @@
 
 
 
+def homeTeam(url):
+    l = []
+    for i in range(1, nbListes()+1):
+        for j in range(1, nbElementsDansChaqueListe()[i-1]+1):
+            l.append(xp.Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[1]/div/span[1]'))
+    return l
+
 def awayTeam(url):
     l = []
     for i in range(1, nbListes()+1):
