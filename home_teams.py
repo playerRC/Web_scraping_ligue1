fe6beb1269fd80c6fbbb510bd877523954f7b34d
RFaska <93133836+playerRC@users.noreply.github.com>
2022-05-06 17:48:30 +0200

    Update home_teams.py

--- home_teams.py
+++ home_teams.py
@@ -1,4 +1,5 @@
 import requests
+import XPath as xp
 from bs4 import BeautifulSoup
 
 #nb de ul dans le div
@@ -20,7 +21,7 @@ def homeTeam(url):
     l = []
     for i in range(1, nbListes()+1):
         for j in range(1, nbElementsDansChaqueListe()[i-1]+1):
-            l.append(Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[1]/div/span[1]'))
+            l.append(xp.Xpath(url, f'/html/body/main/div[3]/div[2]/div/div[2]/ul[{i}]/li[{j}]/a/div[1]/div/span[1]'))
     return l
 
 
