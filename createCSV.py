c2a05edd0c32a5ab49044c9c7b4a18d974ecf190
RFaska <93133836+playerRC@users.noreply.github.com>
2022-05-07 02:26:49 +0200

    Update createCSV.py

--- createCSV.py
+++ createCSV.py
@@ -1,8 +1,30 @@
-with open('matchs.csv', 'wb') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow(['Journée','HomeTeam', 'AwayTeam', 'HG', 'AG'])
-    for i in range(1,39):
-        for j in range(0,10):
-            url = f'https://www.ligue1.fr/calendrier-resultats?seasonId=2020-2021&matchDay={i}'
-            filewriter.writerow([i, homeTeam(url)[j], awayTeam(url)[j], homeGoal(url)[j], awayGoal(url)[j]])
+import csv
+
+header = ['Season','Matchday','HomeTeam', 'AwayTeam', 'HTG', 'ATG']
+with open('all_ligue1_matches.csv', 'w', encoding='UTF8', newline='') as f:
+    fwriter = csv.writer(f)
+    fwriter.writerow(header)
+    for annee in range(1993, 2022):
+        if annee == 2019:
+            # annee covid seulement 28 journees terminees
+            for i in range(1,29):
+                url = f'https://www.ligue1.fr/calendrier-resultats?seasonId=2019-2020&matchDay={i}'
+                for j in range(0,sum(nbElementsDansChaqueListe(url))):
+                    fwriter.writerow(['2019-2020', i, homeTeam(url)[j], awayTeam(url)[j], homeGoal(url)[j], awayGoal(url)[j]])
+        elif annee == 1997 or annee == 1998 or annee == 1999 or annee == 2000 or annee == 2001:
+            # annees ligue 1 composee de 18 equipes donc 34 journees
+            for i in range(1,35):
+                url = f'https://www.ligue1.fr/calendrier-resultats?seasonId={annee}-{annee+1}&matchDay={i}'
+                for j in range(0,sum(nbElementsDansChaqueListe(url))):
+                    fwriter.writerow([f'{annee}-{annee+1}', i, homeTeam(url)[j], awayTeam(url)[j], homeGoal(url)[j], awayGoal(url)[j]])
+        elif annee == 2021:
+            # annee actuel seulement 35 journees terminees le 7 mai 2022
+            for i in range(1,36):
+                url = f'https://www.ligue1.fr/calendrier-resultats?seasonId=2021-2022&matchDay={i}'
+                for j in range(0,sum(nbElementsDansChaqueListe(url))):
+                    fwriter.writerow(['2021-2022', i, homeTeam(url)[j], awayTeam(url)[j], homeGoal(url)[j], awayGoal(url)[j]])
+        else:
+            for i in range(1,39):
+                url = f'https://www.ligue1.fr/calendrier-resultats?seasonId={annee}-{annee+1}&matchDay={i}'
+                for j in range(0,sum(nbElementsDansChaqueListe(url))):
+                    fwriter.writerow([f'{annee}-{annee+1}', i, homeTeam(url)[j], awayTeam(url)[j], homeGoal(url)[j], awayGoal(url)[j]])
