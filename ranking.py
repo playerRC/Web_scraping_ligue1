53cb90e39a40c14d4155787920f7d5f1b8916cc5
RFaska <93133836+playerRC@users.noreply.github.com>
2022-05-09 23:06:07 +0200

    Update ranking.py

--- ranking.py
+++ ranking.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import csv
 
-
+# fct permettant de connaître les équipes ayant participer au moins une fois à la première division française
 def all_teams(df):
     l = []
     for team in df['HomeTeam']:
@@ -9,101 +9,80 @@ def all_teams(df):
             l.append(team)
     return l
 
-''' donne la liste de tous les indices où l'équipe donné en paramètre a joué à domicile ou à l'extérieur '''
-def apparitionTeam(df, team):
-    l=[]
-    for i in range(0, len(df['Season'])):
-        if df['HomeTeam'][i] == team or df['AwayTeam'][i] == team:
-            l.append(i)
-    return l
-
-''' renvoie une liste dont chaque élément correspond au résultat de l'équipe sous forme [V,N,D] 
-    V = victoire , N = match nul , D = défaite
-    avec un deuxième élèment correspondant à [buts marqués, buts concédés]
+''' fct permettant de connaître les résultats d'une équipe donnée sous forme de liste comme suit
+    [ R , GF, GA, Season] avec R = V ou N ou D
+    et GF = buts marqués , GA = buts encaissés
 '''
 def team_result(df, team):
-    m=[]
-    for a in apparitionTeam(df, team):
+    app = []
+    # d'abord on stocke les indices où l'équipe joue un match
+    for i in range(0, len(df['Season'])):
+        if df['HomeTeam'][i] == team or df['AwayTeam'][i] == team:
+            app.append(i)
+    m = []
+    for a in app:
         if (df['ATG'][a] > df['HTG'][a] and df['AwayTeam'][a] == team):
-            l = [[1,0,0], [df['ATG'][a], df['HTG'][a]], df['Season'][a]]
+            l = ['V', [df['ATG'][a], df['HTG'][a]], df['Season'][a]]
         elif (df['ATG'][a] < df['HTG'][a] and df['HomeTeam'][a] == team):
-            l = [[1,0,0], [df['HTG'][a], df['ATG'][a]], df['Season'][a]]
+            l = ['V', [df['HTG'][a], df['ATG'][a]], df['Season'][a]]
         elif (df['ATG'][a] < df['HTG'][a] and df['AwayTeam'][a] == team):
-            l = [[0,0,1], [df['ATG'][a], df['HTG'][a]], df['Season'][a]]
+            l = ['D', [df['ATG'][a], df['HTG'][a]], df['Season'][a]]
         elif (df['ATG'][a] > df['HTG'][a] and df['HomeTeam'][a] == team):
-            l = [[0,0,1], [df['HTG'][a], df['ATG'][a]], df['Season'][a]]
+            l = ['D', [df['HTG'][a], df['ATG'][a]], df['Season'][a]]
         else:
             if df['AwayTeam'][a] == team:
-                l = [[0,1,0], [df['ATG'][a], df['HTG'][a]], df['Season'][a]]
+                l = ['N', [df['ATG'][a], df['HTG'][a]], df['Season'][a]]
             if df['HomeTeam'][a] == team:
-                l = [[0,1,0], [df['HTG'][a], df['ATG'][a]], df['Season'][a]]
+                l = ['N', [df['HTG'][a], df['ATG'][a]], df['Season'][a]]
         m.append(l)
     return m
 
-def nbWins(df, team):
-    nb = 0
-    for result in team_result(df, team):
-        if result[0] == [1,0,0]:
-            nb += 1
-    return nb
-
-def nbDraws(df, team):
-    nb = 0
-    for result in team_result(df, team):
-        if result[0] == [0,1,0]:
-            nb += 1
-    return nb
-
-def nbDefeats(df, team):
-    nb = 0
-    for result in team_result(df, team):
-        if result[0] == [0,0,1]:
-            nb += 1
-    return nb
-
-def nbPoints(df, team):
+''' fct qui renvoie une liste contenant les statistiques d'une équipe donnée '''
+def stats(df, team):
+    nbWins = 0
+    nbDraws = 0
+    nbL = 0
+    GF = 0
+    GA = 0
     pts = 0
-    for s in range(0, len(team_result(df, team))):
-        if team_result(df, team)[s][2] == '1993-1994':
-            pts = 2 * nbWins(df, team) + nbDraws(df, team)
-        else:
-            pts = 3 * nbWins(df, team) + nbDraws(df, team)
-    return pts
-
-def nbGoalsFor(df, team):
-    nb = 0
     for result in team_result(df, team):
-        nb += result[1][0]
-    return nb
-
-def nbGoalsAgainst(df, team):
-    nb = 0
-    for result in team_result(df, team):
-        nb += result[1][1]
-    return nb
-
-def goalAverage(df, team):
-    return nbGoalsFor(df, team) - nbGoalsAgainst(df, team)
+        if result[0] == 'V':
+            nbWins += 1
+            # jusqu'à la saison 1993-1994, une victoire rapportait 2 points
+            if result[0][2] == '1993-1994':
+                pts += 2
+            else:
+                pts += 3
+        if result[0] == 'N':
+            nbDraws += 1
+            pts += 1
+        if result[0] == 'D':
+            nbL += 1
+        GF += result[1][0]
+        GA += result[1][1]
+    
+    diff = GF-GA
+    return [nbWins, nbDraws, nbL, GF, GA, diff, pts]
 
 def all_stats(df):
     l = []
     for team in all_teams(df):
         m = []
         m.append(team)
-        m.append(len(apparitionTeam(df, team)))
-        m.append(nbWins(df, team))
-        m.append(nbDraws(df, team))
-        m.append(nbDefeats(df, team))
-        m.append(nbGoalsFor(df, team))
-        m.append(nbGoalsAgainst(df, team))
-        m.append(goalAverage(df, team))
-        m.append(nbPoints(df, team))
+        m.append(len(team_result(df,team)))
+        m.append(stats(df, team)[0])
+        m.append(stats(df, team)[1])
+        m.append(stats(df, team)[2])
+        m.append(stats(df, team)[3])
+        m.append(stats(df, team)[4])
+        m.append(stats(df, team)[5])
+        m.append(stats(df, team)[6])
         l.append(m)
     l.sort(key=lambda row: (row[8], row[7], row[5], row[6]), reverse = True)
     return l
 
     
-# Read CSV file into DataFrame df
+# lecture du fichier CSV --> DataFrame df
 df = pd.read_csv('all_ligue1_matches.csv')
 
 header = ['Rank','Team','Games','Wins','Draws','Defeats','GF','GA','Diff','Points']
@@ -114,5 +93,3 @@ with open('historical_ranking.csv', 'w', encoding='UTF-8', newline='') as f:
     for stat in all_stats(df):
         fwriter.writerow([i, stat[0], stat[1], stat[2], stat[3], stat[4], stat[5], stat[6], stat[7], stat[8]])
         i += 1
-
-
